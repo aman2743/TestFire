39532ad592586dce688416b435d2ea509c44eda6
Aman Kumar <eraman.kush@gmail.com>
2025-01-18 12:47:24 +0530

    - Added code for For loop

---
+++ src/main/java/LoopInJava/ForLoopTesting.java
@@ -0,0 +1,9 @@
+package LoopInJava;
+
+public class ForLoopTesting {
+    public static void main(String[] args) {
+        for (int i=0;i<10;i++){
+            System.out.println(i);
+        }
+    }
+}
