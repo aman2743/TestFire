bd562684c895c5c5d72e0f3d1b821ca373b516e1
Aman Kumar <eraman.kush@gmail.com>
2025-01-25 18:59:31 +0530

    - Added code inside multiple section of java

---
+++ src/main/java/ArrayInJava/firstArrayProgram.java
@@ -0,0 +1,11 @@
+package ArrayInJava;
+
+public class firstArrayProgram {
+    public static void main(String[] args) {
+       int arr[]={2,1,6,4,8,3,9};
+        char []arr1={'a','c','d','h'};
+        String [] arr3={"Test","QA","Ram"};
+        boolean b[]={true,false};
+       
+    }
+}
